0a50102fe96ebcef6934aa92c3e67c49f1b72d3a
edemir2 <edemir2@binghamton.edu>
2024-07-26 11:06:29 +0300

    Initial project setup with Spring Boot dependencies and configuration

---
+++ src/main/java/com/turkcell/internProject/InternProjectApplication.java
@@ -0,0 +1,15 @@
+package com.turkcell.internProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class InternProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternProjectApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/turkcell/internProject/InternProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turkcell.internProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
